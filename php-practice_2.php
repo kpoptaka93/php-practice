4b5bb69d93c2399c75f2578a6392427dc7317d20
杉浦　天晴 <taka@sugiuraapparenoMacBook-Air.local>
2026-08-26 12:08:49 +0900

    インデント修正

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,107 +1,106 @@
 <?php
-
 // Q1 tic-tac問題
-
 for($i = 1; $i <= 100; $i++)
-
-{if($i % 4 === 0 && $i % 5 === 0)
-
 {
+    if($i % 4 === 0 && $i % 5 === 0)
+  {
+      echo 'tic-tac';
+  }
+   elseif($i % 4 === 0)
+  {
+      echo "tic\n";
+  }
+   elseif($i % 5 === 0)
+  {
+      echo "tac\n";
+  }
+   else
+  {
+      echo $i . "\n";
 
-echo 'tic-tac';
-
+  }
 }
 
-elseif($i % 4 === 0)
 
-{
-
-echo "tic\n";
-
-}
-
-elseif($i % 5 === 0)
-
-{
-
-echo "tac\n";
-
-}
-
-else
-
-echo $i . "\n";
-
-}
 
 // Q2 多次元連想配列
 
-echo 'Bさんの電話番号は、' . $personalInfos[1]['tel'] . "です。\n";
+echo 'Bさんの電話番号は、' . $personalInfos[1]['tel'] . "です。\n"; 
 
-foreach($personalInfos as $x => $y){
+foreach($personalInfos as $x => $y)
+{
+   $num = $x + 1;
 
-$num = $x + 1;
+  echo $num . '番目の' . $y['name'] . 'のメールアドレスは' . $y['mail'] . 'で、電話番号は' . $y['tel'] . "です。\n";
+}
 
-echo $num . '番目の' . $y['name'] . 'のメールアドレスは' . $y['mail'] . 'で、電話番号は' . $y['tel'] . "です。\n";
+   $ageList = [20, 30, 18];
+
+foreach($personalInfos as $x => $y)
+{
+
+   $personalInfos[$x]['age'] = $ageList[$x];
 
 }
 
-$ageList = [20, 30, 18];
+var_dump($personalInfos); 
 
-foreach($personalInfos as $x => $y){
 
-$personalInfos[$x]['age'] = $ageList[$x];
 
-}
-
-var_dump($personalInfos);
 
 // Q3 オブジェクト-1
+ $yamada = new Student(120, '山田');
 
-$yamada = new Student(120, '山田');
 
-echo '学籍番号は' . $yamada->studentId . '番の生徒は' . $yamada->studentName . 'です。';
+ echo '学籍番号は' . $yamada->studentId . '番の生徒は' . $yamada->studentName . 'です。'; 
+
+
 
 // Q4 オブジェクト-2
-
-class Student
-
+ class Student
 {
+     public $studentId;
+     public $studentName;
 
-public $studentId;
+     public function __construct($id, $name)
+   {
+         $this->studentId = $id;
+         $this->studentName = $name;
+    }
 
-public $studentName;
+     public function attend($subject)
+   {
+         echo $this->studentName . 'は' . $subject . 'の授業に参加しました' . '学績番号:' . $this->studentId ; 
+    }
 
-public function __construct($id, $name)
 
-{
-
-$this->studentId = $id;
-
-$this->studentName = $name;
-
+        
 }
+     $yamada->attend('PHP');
 
-public function attend($subject)
 
-{
+ 
 
-echo $this->studentName . 'は' . $subject . 'の授業に参加しました' . '学績番号:' . $this->studentId ;
-
-$yamada->attend('PHP');
 
 // Q5 定義済みクラス
 
-$date = new DateTime('-1 month');
 
-var_dump($date);
+ $date = new DateTime('-1 month');
 
-$today = new DateTime('now');
+ var_dump($date);
 
-$past = new DateTime('1992-04-25');
 
-$interval = $today->diff($past);
 
-echo 'あの日から' . $interval->format('%a') . '日経過しました。';
+ $today = new DateTime('now');
+ $past = new DateTime('1992-04-25'); 
+ $interval = $today->diff($past);
+
+
+
+ echo 'あの日から' . $interval->format('%a') . '日経過しました。';
+
+
+
+
 
 ?>
